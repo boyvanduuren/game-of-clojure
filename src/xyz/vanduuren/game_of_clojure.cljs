6a7708b6e868ba5572ad44f7703d8d21348130c8
Boy van Duuren <bvanduuren@schubergphilis.com>
2018-10-27 23:46:26 +0200

    In need of a heavy refactor

--- src/xyz/vanduuren/game_of_clojure.cljs
+++ src/xyz/vanduuren/game_of_clojure.cljs
@@ -10,7 +10,7 @@
 (def tile-size 10)
 (def color-on "black")
 (def color-off "white")
-(def grid-height 30)
+(def grid-height 40)
 (def grid-width 100)
 
 (defn tile-color [bit]
@@ -115,16 +115,27 @@
         (range 0 height))))
 
 (def empty-board (vec (repeat grid-height (vec (repeat grid-width 0)))))
-(def board [
-           [0 1 0 0]
-           [1 0 1 1]
-           [0 0 0 1]])
+(defn build-board [coords]
+  (defn build-board-inter [coords board]
+    (if (empty? coords) board
+        (build-board-inter (rest coords)
+                           (let [coord (first coords)
+                                 y (first coord)
+                                 x (second coord)
+                                 row (nth board y)]
+                             (assoc board y (assoc row x 1))))))
 
-(.log js/console (str (next-generation board)))
+  (build-board-inter coords empty-board))
+(def gosper '((5 1) (5 2) (6 1) (6 2) (5 11) (6 11) (7 11) (4 12) (3 13) (3 14) (8 12) (9 13) (9 14) (6 15) (4 16) (5 17) (6 17) (7 17) (6 18) (8 16) (3 21) (4 21) (5 21) (3 22) (4 22) (5 22) (2 23) (6 23) (1 25) (2 25) (6 25) (7 25) (3 35) (4 35) (3 36) (4 36)))
+(def board (build-board gosper))          
+;;(def board [
+;;           [0 1 0 0]
+;;           [1 0 1 1]
+;;           [0 0 0 1]])
 
 (defn main-loop [state]
   (draw-board! state)
-  (js/setTimeout #(main-loop (next-generation state)) 1000))
+  (js/setTimeout #(main-loop (next-generation state)) 100))
 
 (main-loop board)
 
